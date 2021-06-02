6b5a72e376525baf599510c4ea9de9ae23c5fc0f
UnoHvH <84003316+UnoHvH@users.noreply.github.com>
2021-06-02 19:41:22 +1000

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "ODQ5NTgyMjAyNDEyOTkwNDY0.YLdRFQ.WGAGTCjZYLlU4hLH6n0EXunQDTU",
+    prefix: "cb.",
+    admins: [
+        "537981008725671938"
+],
+    debug: true,
+    countChannel: "countChannelID"
+};
